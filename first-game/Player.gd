d960db7c17279fcdc3c5b2fa9a0b0402f5ec120e
Vinson Beduya <vinsonemb.151994@gmail.com>
2021-11-07 22:58:40 +1300

    Following godot first game tutorial. Finished player movement and animation.

---
+++ first-game/Player.gd
@@ -0,0 +1,42 @@
+extends Area2D
+
+export var speed = 400
+var screen_size
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+
+func _process(delta):
+	player_movement(delta)
+	
+func player_movement(delta):
+	var velocity = Vector2()
+	
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite.play()
+	else:
+		$AnimatedSprite.stop()
+	position += velocity * delta
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
+	flip_sprite_animation(velocity)
+	
+func flip_sprite_animation(velocity):
+	if velocity.x != 0:
+		$AnimatedSprite.animation = "walk"
+		$AnimatedSprite.flip_v = false
+		$AnimatedSprite.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite.animation = "up"
+		$AnimatedSprite.flip_v = velocity.y > 0
+	
